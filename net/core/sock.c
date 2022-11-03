b10fcafe080616a3b0c9950e46b59b326647e688
rajatthelegend <rajatbillava50@gmail.com>
2022-11-03 11:40:31 +0100

    Init M515FXXU4DVH2

--- net/core/sock.c
+++ net/core/sock.c
@@ -662,7 +662,6 @@ out:
 	return ret;
 }
 
-#ifdef CONFIG_KNOX_NCM
 /* START_OF_KNOX_NPA */
 /** The function sets the domain name associated with the socket. **/
 static int sock_set_domain_name(struct sock *sk, char __user *optval, int optlen)
@@ -749,7 +748,6 @@ out:
 }
 
 /* END_OF_KNOX_NPA */
-#endif
 
 static inline void sock_valbool_flag(struct sock *sk, int bit, int valbool)
 {
@@ -798,8 +796,7 @@ int sock_setsockopt(struct socket *sock, int level, int optname,
 
 	if (optname == SO_BINDTODEVICE)
 		return sock_setbindtodevice(sk, optval, optlen);
-
-#ifdef CONFIG_KNOX_NCM
+	
 	/* START_OF_KNOX_NPA */
 	if (optname == SO_SET_DOMAIN_NAME)
 		return sock_set_domain_name(sk, optval, optlen);
@@ -808,7 +805,6 @@ int sock_setsockopt(struct socket *sock, int level, int optname,
 	if (optname == SO_SET_DNS_PID)
 		return sock_set_dns_pid(sk, optval, optlen);
 	/* END_OF_KNOX_NPA */
-#endif
 
 	if (optlen < sizeof(int))
 		return -EINVAL;
@@ -1623,8 +1619,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 		      struct proto *prot, int kern)
 {
 	struct sock *sk;
-
-#ifdef CONFIG_KNOX_NCM
+	
 	/* START_OF_KNOX_NPA */
 	struct pid *pid_struct = NULL;
 	struct task_struct *task = NULL;
@@ -1635,12 +1630,10 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 	int parent_returnValue = -1;
 	char full_parent_process_name[PROCESS_NAME_LEN_NAP] = {0};
 	/* END_OF_KNOX_NPA */
-#endif
 
 	sk = sk_prot_alloc(prot, priority | __GFP_ZERO, family);
 	if (sk) {
 		sk->sk_family = family;
-#ifdef CONFIG_KNOX_NCM
 		/* START_OF_KNOX_NPA */
 		/* assign values to members of sock structure when npa flag is present */
 		sk->knox_uid = current->cred->uid.val;
@@ -1684,8 +1677,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 			}
 		}
 		/* END_OF_KNOX_NPA */
-#endif
-
+		
 		sk->sk_prot = sk->sk_prot_creator = prot;
 		sk->sk_kern_sock = kern;
 		sock_lock_init(sk);
